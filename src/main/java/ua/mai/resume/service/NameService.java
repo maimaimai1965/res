738347ab0eaf5d5424258ed7ac81bd86333bc8ba
mai <maimaimai1965@gmail.com>
2016-04-30 15:24:15 +0300

    Create project

---
+++ src/main/java/ua/mai/resume/service/NameService.java
@@ -0,0 +1,17 @@
+package ua.mai.resume.service;
+
+/**
+ * 
+ * @author devstudy
+ * @see http://devstudy.net
+ */
+public class NameService {
+
+	public static NameService getInstance(){
+		return new NameService();
+	}
+
+	public String convertName(String name) {
+		return name.toUpperCase();
+	}
+}
