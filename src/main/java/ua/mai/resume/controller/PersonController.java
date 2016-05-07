5a4b8ac1a5698c4914362e518e2beabc97e21d0d
mai <maimaimai1965@gmail.com>
2016-05-07 19:13:14 +0300

    Lesson 05a

--- src/main/java/ua/mai/resume/controller/PersonController.java
+++ src/main/java/ua/mai/resume/controller/PersonController.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/profile")
+@WebServlet("/person")
 public class PersonController extends HttpServlet{
 
 	private static final long serialVersionUID = -2007940300578601854L;
